9ace1c3653cb0a13cafa95b1a7e8ecc4113abdb3
Yang <ybh1760@gmail.com>
2019-10-12 00:33:03 +0900

    reset & confirm버튼 기능구현

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -14,10 +14,31 @@ import Input from "../components/Input";
 
 const StartGameScreen = props => {
   const [enteredNumber, setEnteredNumber] = useState("");
+  const [selectedNumber, setSelectedNumber] = useState();
+  const [confirmed, setConfirmed] = useState(false);
 
   const numberInputHandler = number => {
     setEnteredNumber(number.replace(/[^0-9]/g, ""));
   };
+
+  const resetInputHandler = () => {
+    setConfirmed(false);
+    setEnteredNumber("");
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredNumber);
+    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chosenNumber);
+    setEnteredNumber("");
+  };
+  let confirmedOutput;
+  if (confirmed) {
+    confirmedOutput = <Text>Chosen Number:{selectedNumber}</Text>;
+  }
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.screen}>
@@ -35,13 +56,22 @@ const StartGameScreen = props => {
           />
           <View style={styles.buttonContainer}>
             <View style={styles.button}>
-              <Button title="Reset" color={Colors.accent} />
+              <Button
+                title="Reset"
+                color={Colors.accent}
+                onPress={resetInputHandler}
+              />
             </View>
             <View style={styles.button}>
-              <Button title="Confirm" color={Colors.primary} />
+              <Button
+                title="Confirm"
+                color={Colors.primary}
+                onPress={confirmInputHandler}
+              />
             </View>
           </View>
         </Card>
+        {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
   );
