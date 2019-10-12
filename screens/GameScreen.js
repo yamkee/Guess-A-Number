8e8319f0978f52d587c85f9b0df38f6cd06141d5
Yang <ybh1760@gmail.com>
2019-10-12 14:17:03 +0900

    GameScreen의 ui 구성

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -18,8 +21,32 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Number</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <Button title="LOWER" onPress={() => {}} />
+        <Button title="GREATER" onPress={() => {}} />
+      </Card>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20
+  },
+  buttonContainer: {
+    width: 300,
+    maxWidth: "80%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20
+  }
+});
 
 export default GameScreen;
